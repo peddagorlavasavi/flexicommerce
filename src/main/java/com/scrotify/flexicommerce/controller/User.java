2da251564f37525e6c8aa9c8e081630a2f5e207f
peddagorlavasavi <vasavi19932@gmail.com>
2019-12-23 12:32:31 +0530

    User entity

---
+++ src/main/java/com/scrotify/flexicommerce/controller/User.java
@@ -0,0 +1,23 @@
+package com.scrotify.flexicommerce.controller;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Setter
+@Getter
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer userId;
+	private String password;
+	private String usernName;
+
+}
